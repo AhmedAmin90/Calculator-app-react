be301645e30bcb2013649c2ae11c3ba557a04df6
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-07-09 16:57:57 +0300

    Add pages with routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,31 @@
-import React, { useState} from 'react';
-import PropTypes from 'prop-types';
-import ButtonPanel from './ButtonPanel';
-import Display from './Display';
-import calculate from '../logic/calculate';
+import React from 'react'
+import Calculator from './Calculator';
+import About from './About';
+import Quote from './Quote';
 import '../App.css';
+import {
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 
 
 const App = ()=> {
-  const [value, setValue] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-  const handleClick = (name) => {
-    const data = { ...value };
-    const result = calculate(data, name);
-    setValue({ ...result });
-  };
-
-  const { total, next } = value;
-  const result = next ? next && next.toString() : total && total.toString();
     return (
       <div className="App">
-        <h1>Calculator Application</h1>
-        <Display result={result} />
-        <ButtonPanel clickHandler={handleClick} />
+        <nav>
+          <NavLink to="/" > Home </NavLink>
+          <NavLink to="/calculator" > Calculator </NavLink>
+          <NavLink to="/quote" > Quote </NavLink>
+        </nav>
+        <Switch>
+           <Route exact path="/" render= {()=> <About />} />
+           <Route exact path="/calculator" render= {()=> <Calculator />} />
+           <Route exact path="/quote" render={()=> <Quote />} />
+        </Switch>
+       
+
       </div>)
 }
 export default App;
 
-Display.propTypes = {
-  result: PropTypes.string,
-};
-
-ButtonPanel.propTypes = {
-  clickHandler: PropTypes.func.isRequired,
-};
---
+++ src/components/Quote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+
+const Quote = ()=> {
+    return(
+        <div className="About">
+        <div className="hero-section-text">
+        <h1>Quote of the day</h1>
+        <p className="my-description">
+        Some people want it to happen, some wish it would happen, others make it happen.
+
+        </p>
+    </div>
+    </div>
+    )
+}
+
+export default Quote;
