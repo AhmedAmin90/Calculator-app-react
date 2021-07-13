8b004242d314ad67f83cca064e5a24633ceb30ca
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-07-13 13:36:19 +0300

    Add tests for untested features in calculate function , and add negative tests

--- src/__test__/logic/calculate.test.js
+++ src/__test__/logic/calculate.test.js
@@ -15,6 +15,29 @@ describe('calculate', () => {
     expect(typeof result.total).toEqual('string');
   });
 
+  it('Total total will be 0 if total and next are null and click on operation buttons', () => {
+    const result = calculate({ total: null , next: null, operation: null }, '+');
+    expect(result.total).toEqual('0');
+    expect(result.next).toEqual(null);
+  });
+
+  it('If  operation, number will be added to the next', () => {
+    const result = calculate({ total: '6', next: null, operation: '+' }, '9');
+    expect(result.total).toEqual('6');
+    expect(result.next).toEqual('9');
+  });
+
+  it('If next and operation is null , number will be added to the total', () => {
+    const result = calculate({ total: '6', next: null, operation: null }, '9');
+    expect(result.total).toEqual('69');
+  });
+
+  it('If  operation, number will be added to the next', () => {
+    const result = calculate({ total: '6', next: null, operation: '+' }, '9');
+    expect(result.total).toEqual('6');
+    expect(result.next).toEqual('9');
+  });
+
   it('AC return null for all data', () => {
     const result = calculate({ total: null, next: null, operation: null }, 'AC');
     expect(result).toEqual({ next: null, operation: null, total: null });
@@ -33,4 +56,19 @@ describe('calculate', () => {
     const result = calculate({ total: '6', next: null, operation: null }, '%');
     expect(result).toEqual({ next: null, operation: null, total: 0.06 });
   });
+  it('"=" return total if next is null ', () => {
+    const result = calculate({ total: '6', next: null, operation: null }, '=');
+    expect(result.total).toEqual('6');
+  });
+  it('"=" return 0 if next and total are null ', () => {
+    const result = calculate({ total: null , next: null, operation: null }, '=');
+    expect(result.total).toEqual('0');
+  });
+  it('"=" return total equal to operation of next, total and operation return to null ', () => {
+    const result = calculate({ total: '5' , next: '6', operation: 'X' }, '=');
+    expect(result.total).toEqual('30');
+    expect(result.next).toEqual(null);
+    expect(result.operation).toEqual(null);
+  });
+
 });
--- src/__test__/logic/operate.test.js
+++ src/__test__/logic/operate.test.js
@@ -10,23 +10,27 @@ describe(' Operate', () => {
     expect(typeof operate).toEqual('function');
   });
 
-  it('sum two numbers', () => {
+  it('sum two numbers , and do not return null', () => {
     const result = operate('2', '4', '+');
     expect(result).toBe('6');
+    expect(result).not.toBe(null);
   });
 
-  it('sustrate two numbers value', () => {
+  it('sustrate two numbers value , and do not return null', () => {
     const result = operate('2', '4', '-');
     expect(result).toEqual('-2');
+    expect(result).not.toBe(null);
   });
 
-  it('divide two numbers', () => {
+  it('divide two numbers , and do not return null', () => {
     const result = operate('2', '4', '÷');
     expect(result).toEqual('0.5');
+    expect(result).not.toBe(null);
   });
 
-  it('multiple two numbers', () => {
+  it('multiple two numbers , and do not return null', () => {
     const result = operate('2', '4', 'X');
     expect(result).toEqual('8');
+    expect(result).not.toBe(null);
   });
 });
